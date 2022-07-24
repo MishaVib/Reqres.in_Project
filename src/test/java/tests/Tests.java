71edfdff54126647fb08c10db2923483a3935f2b
Misha_Vib <nsutkov382@gmail.com>
2022-07-25 00:09:34 +0300

    fixed import

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.is;
 import static specs.Specs.*;
 
 public class Tests {
